622f757bc8c739a8fd69a9687531691eefea5e69
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-08-26 22:00:49 +0100

    refactored tests

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,44 +8,60 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Bank Account")
 public class BankAccountTest {
-    BankAccount bankAccount;
-
-    @BeforeEach
-    void setup(){
-        bankAccount = new BankAccount();
-    }
 
     @Test
     @DisplayName("can create an account with zero balance")
     void testCanCreateABlankAccount(){
+        BankAccount bankAccount = new BankAccount();
         assertEquals(0, bankAccount.balance);
     }
 
-    @Test
-    @DisplayName("can deposit money into account")
-    void testCanDepositMoneyIntoAccount(){
-        bankAccount.deposit(1000);
-        assertEquals(1000, bankAccount.balance);
+    @Nested
+    @DisplayName("Deposits")
+    class Deposits{
+        BankAccount bankAccount;
+        @BeforeEach
+        void setup(){
+            bankAccount = new BankAccount();
+        }
+
+        @Test
+        @DisplayName("can deposit money into account")
+        void testCanDepositMoneyIntoAccount(){
+            bankAccount.deposit(1000);
+            assertEquals(1000, bankAccount.balance);
+        }
+
+        @Test
+        @DisplayName("cannot deposit negative amount of money into account")
+        void testCannotDepositNegativeMoney(){
+            assertThrows(ArithmeticException.class, () -> bankAccount.deposit(-100));
+        }
     }
 
-    @Test
-    @DisplayName("cannot deposit negative amount of money into account")
-    void testCannotDepositNegativeMoney(){
-        assertThrows(ArithmeticException.class, () -> bankAccount.deposit(-100));
-    }
 
-    @Test
-    @DisplayName("can withdraw money from account")
-    void testCanWithdrawMoney(){
-        bankAccount.deposit(1000);
-        bankAccount.withdraw(200);
-        assertEquals(800, bankAccount.balance);
-    }
+    @Nested
+    @DisplayName("Withdrawals")
+    class Withdrawals{
+        BankAccount bankAccount;
 
-    @Test
-    @DisplayName("cannot withdraw negative amount of money into account")
-    void testCannotWithdrawNegativeAmount(){
-        bankAccount.deposit(100);
-        assertThrows(ArithmeticException.class, () -> bankAccount.withdraw(-500));
+        @BeforeEach
+        void setup(){
+            bankAccount = new BankAccount();
+            bankAccount.deposit(1000);
+        }
+        @Test
+        @DisplayName("can withdraw money from account")
+        void testCanWithdrawMoney(){
+            bankAccount.withdraw(200);
+            assertEquals(800, bankAccount.balance);
+        }
+
+        @Test
+        @DisplayName("cannot withdraw negative amount of money into account")
+        void testCannotWithdrawNegativeAmount(){
+            bankAccount.deposit(100);
+            assertThrows(ArithmeticException.class, () -> bankAccount.withdraw(-500));
+        }
     }
 }
